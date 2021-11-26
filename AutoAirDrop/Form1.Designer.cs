7383cbc293b7d62fbfd4dfb1cd8e932b461251a6
NamNH <hoainam.fptu@gmail.com>
2021-11-26 09:45:39 +0700

    Add project files.

---
+++ AutoAirDrop/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace AutoAirDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgAccount = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telegram = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.twitter = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dtgAirdrop = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.reference = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtgResult = new System.Windows.Forms.DataGridView();
+            this.accountID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.airdropId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgAccount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgAirdrop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgAccount
+            // 
+            this.dtgAccount.AllowUserToAddRows = false;
+            this.dtgAccount.AllowUserToDeleteRows = false;
+            this.dtgAccount.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgAccount.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.telegram,
+            this.twitter});
+            this.dtgAccount.Location = new System.Drawing.Point(12, 53);
+            this.dtgAccount.Name = "dtgAccount";
+            this.dtgAccount.ReadOnly = true;
+            this.dtgAccount.RowHeadersVisible = false;
+            this.dtgAccount.RowHeadersWidth = 51;
+            this.dtgAccount.RowTemplate.Height = 24;
+            this.dtgAccount.Size = new System.Drawing.Size(500, 169);
+            this.dtgAccount.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 40;
+            // 
+            // telegram
+            // 
+            this.telegram.HeaderText = "telegram";
+            this.telegram.MinimumWidth = 6;
+            this.telegram.Name = "telegram";
+            this.telegram.ReadOnly = true;
+            this.telegram.Width = 107;
+            // 
+            // twitter
+            // 
+            this.twitter.HeaderText = "twitter";
+            this.twitter.MinimumWidth = 6;
+            this.twitter.Name = "twitter";
+            this.twitter.ReadOnly = true;
+            this.twitter.Width = 107;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(921, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dtgAirdrop
+            // 
+            this.dtgAirdrop.AllowUserToAddRows = false;
+            this.dtgAirdrop.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgAirdrop.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.reference});
+            this.dtgAirdrop.Location = new System.Drawing.Point(12, 286);
+            this.dtgAirdrop.Name = "dtgAirdrop";
+            this.dtgAirdrop.RowHeadersVisible = false;
+            this.dtgAirdrop.RowHeadersWidth = 51;
+            this.dtgAirdrop.RowTemplate.Height = 24;
+            this.dtgAirdrop.Size = new System.Drawing.Size(500, 244);
+            this.dtgAirdrop.TabIndex = 6;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ID";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "type";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "name";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // reference
+            // 
+            this.reference.HeaderText = "ref link";
+            this.reference.MinimumWidth = 6;
+            this.reference.Name = "reference";
+            this.reference.Width = 125;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(175, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Airdrop List";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(206, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Account";
+            // 
+            // dtgResult
+            // 
+            this.dtgResult.AllowUserToAddRows = false;
+            this.dtgResult.AllowUserToDeleteRows = false;
+            this.dtgResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.accountID,
+            this.airdropId,
+            this.status});
+            this.dtgResult.Location = new System.Drawing.Point(554, 53);
+            this.dtgResult.Name = "dtgResult";
+            this.dtgResult.ReadOnly = true;
+            this.dtgResult.RowHeadersVisible = false;
+            this.dtgResult.RowHeadersWidth = 51;
+            this.dtgResult.RowTemplate.Height = 24;
+            this.dtgResult.Size = new System.Drawing.Size(270, 477);
+            this.dtgResult.TabIndex = 9;
+            // 
+            // accountID
+            // 
+            this.accountID.HeaderText = "account id";
+            this.accountID.MinimumWidth = 6;
+            this.accountID.Name = "accountID";
+            this.accountID.ReadOnly = true;
+            this.accountID.Width = 75;
+            // 
+            // airdropId
+            // 
+            this.airdropId.HeaderText = "airdrop id";
+            this.airdropId.MinimumWidth = 6;
+            this.airdropId.Name = "airdropId";
+            this.airdropId.ReadOnly = true;
+            this.airdropId.Width = 75;
+            // 
+            // status
+            // 
+            this.status.HeaderText = "status";
+            this.status.MinimumWidth = 6;
+            this.status.Name = "status";
+            this.status.ReadOnly = true;
+            this.status.Width = 60;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(650, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1105, 561);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtgResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtgAirdrop);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dtgAccount);
+            this.Name = "Form1";
+            this.Text = "Airdrop V1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgAccount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgAirdrop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgAccount;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dtgAirdrop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn reference;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telegram;
+        private System.Windows.Forms.DataGridViewTextBoxColumn twitter;
+        private System.Windows.Forms.DataGridView dtgResult;
+        private System.Windows.Forms.DataGridViewTextBoxColumn accountID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn airdropId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AutoAirDrop/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutoAirDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        #region
+        private void loadFormAccount()
+        {
+           /* danh sách tài khoản hiển thị ở đây
+            * ID là id của tài khoản
+            * twitter
+            * telegram
+            */
+            if (!File.Exists("Account.txt"))
+            {
+                Console.WriteLine("File does not exist");
+                return;
+            }
+
+            List<string> stringList = new List<string>();
+            using (StreamReader streamReader = new StreamReader("Account.txt"))
+            {
+                string str;
+                while ((str = streamReader.ReadLine()) != null)
+                {
+                    this.dtgAccount.Rows.Add(
+                        (object)str.Split('|')[0],
+                        (object)str.Split('|')[1],
+                        (object)str.Split('|')[2]);
+                }
+            }
+        }
+
+        private void loadFormAirdrop()
+        {
+            /* danh sách airdrop hiển thị ở đây. 
+             * Trong đó sắp xếp theo ID của airdrop. 
+             * Type - là thể loại reg: gleam or normal.
+             * Name - của airdrop token: momo, ksb, shib,...
+             * Ref link - là link chính để các account còn lại ref
+             * Data được load lên từ file .txt
+             */
+            if (!File.Exists("Airdrop.txt"))
+            {
+                Console.WriteLine("File does not exist");
+                return;
+            }
+
+            List<string> stringList = new List<string>();
+            using (StreamReader streamReader = new StreamReader("Airdrop.txt"))
+            {
+                string str;
+                while ((str = streamReader.ReadLine()) != null)
+                {
+                    this.dtgAirdrop.Rows.Add(
+                        (object)str.Split('|')[0],
+                        (object)str.Split('|')[1],
+                        (object)str.Split('|')[2],
+                        (object)str.Split('|')[3]);
+                }
+            }
+
+        }
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadFormAccount();
+            loadFormAirdrop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Cần mở được profile theo id account
+        }
+    }
+}
